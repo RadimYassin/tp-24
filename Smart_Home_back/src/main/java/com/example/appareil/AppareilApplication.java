9ae2ff303ba7456760850a11a401cfa8aa3db52d
RadimYassin <radimyassin20@gmail.com>
2026-01-01 15:19:44 +0100

    test

---
+++ Smart_Home_back/src/main/java/com/example/appareil/AppareilApplication.java
@@ -0,0 +1,19 @@
+package com.example.appareil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Smart Home System Application.
+ * Main entry point for the professional home automation platform.
+ *
+ * @author Radim Yassine
+ * @version 1.0.0
+ */
+@SpringBootApplication
+public class AppareilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppareilApplication.class, args);
+    }
+}
